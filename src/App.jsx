836ef1ff9968db6e799a959b019807f8964299f4
Ajithkumar-M01 <ajithkumaran.srk@gmail.com>
2024-11-08 23:39:08 +0530

    minor edit

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
     <>
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm">
+          <div className="bg-white p-6 rounded-lg shadow-lg sm:-mt-48">
             <button
               className="block ml-auto text-red-500 hover:text-gray-200 bg-gray-200 hover:bg-red-500 px-2 rounded-full"
               onClick={closeModal}
